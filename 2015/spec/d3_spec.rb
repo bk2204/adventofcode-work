dbc6a7952615f0094dce8acd46207bdc51041cc6
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-06 17:24:40 +0000

    Add second part of day 3.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d3_spec.rb
+++ 2015/spec/d3_spec.rb
@@ -2,15 +2,21 @@ require 'rspec'
 load File.join(File.dirname(__FILE__), '..', 'd3')
 
 describe AdventOfCode::D3::Processor do
-  def process(s)
-    p = AdventOfCode::D3::Processor.new
+  def process(nprocs, s)
+    p = AdventOfCode::D3::Demuxer.new(nprocs)
     p.process(s)
     p.locations
   end
 
-  it 'should process testcases correctly' do
-    expect(process('>').length).to eq 2
-    expect(process('^>v<').length).to eq 4
-    expect(process('^v^v^v^v^v').length).to eq 2
+  it 'should process single-Santa testcases correctly' do
+    expect(process(1, '>').length).to eq 2
+    expect(process(1, '^>v<').length).to eq 4
+    expect(process(1, '^v^v^v^v^v').length).to eq 2
+  end
+
+  it 'should process multiple-Santa testcases correctly' do
+    expect(process(2, '^v').length).to eq 3
+    expect(process(2, '^>v<').length).to eq 3
+    expect(process(2, '^v^v^v^v^v').length).to eq 11
   end
 end
