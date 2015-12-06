3a13f77ca3db042ae867cb29e9b7a81f630c0f56
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-06 17:03:19 +0000

    Add second task of day 2 puzzle.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d2_spec.rb
+++ 2015/spec/d2_spec.rb
@@ -1,24 +1,36 @@
 require 'rspec'
 load File.join(File.dirname(__FILE__), '..', 'd2')
 
-describe AdventOfCode::D2::Processor do
-  it 'should compute testcases correctly' do
+describe AdventOfCode::D2::Present do
+  it 'should compute paper testcases correctly' do
     [
       [2, 3, 4, 58],
       [1, 1, 10, 43]
     ].each do |(d1, d2, d3, total)|
-      p = AdventOfCode::D2::Processor.new(d1, d2, d3)
-      expect(p.total).to eq total
+      p = AdventOfCode::D2::Present.new(d1, d2, d3)
+      expect(p.total_paper).to eq total
     end
   end
 
-  it 'automatically sort items' do
+  it 'should automatically sort items' do
     [
       [3, 4, 2, 58],
       [1, 10, 1, 43]
     ].each do |(d1, d2, d3, total)|
-      p = AdventOfCode::D2::Processor.new(d1, d2, d3)
-      expect(p.total).to eq total
+      p = AdventOfCode::D2::Present.new(d1, d2, d3)
+      expect(p.total_paper).to eq total
+    end
+  end
+
+  it 'should compute ribbon testcases correctly' do
+    [
+      [3, 4, 2, 10, 24, 34],
+      [1, 10, 1, 4, 10, 14]
+    ].each do |(d1, d2, d3, dist, vol, total)|
+      p = AdventOfCode::D2::Present.new(d1, d2, d3)
+      expect(p.shortest_distance).to eq dist
+      expect(p.volume).to eq vol
+      expect(p.total_ribbon).to eq total
     end
   end
 end
