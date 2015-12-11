2d2d7dadcce8eaa31e2c53052e7b8b708dff3321
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-11 23:40:41 +0000

    Add part 2 of day 5.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d5_spec.rb
+++ 2015/spec/d5_spec.rb
@@ -6,6 +6,10 @@ describe AdventOfCode::D5::Classifier do
     AdventOfCode::D5::Classifier.new.nice?(s)
   end
 
+  def new_nice?(s)
+    AdventOfCode::D5::Classifier.new.new_nice?(s)
+  end
+
   it 'should detect nice strings correctly' do
     expect(nice?('ugknbfddgicrmopn')).to eq true
     expect(nice?('aaa')).to eq true
@@ -21,5 +25,18 @@ describe AdventOfCode::D5::Classifier do
 
   it 'should reject strings with insufficient vowels' do
     expect(nice?('dvszwmarrgswjxmb')).to eq false
+  end
+
+  it 'should detect new nice strings correctly' do
+    expect(new_nice?('qjhvhtzxzqqjkmpb')).to eq true
+    expect(new_nice?('xxyxx')).to eq true
+  end
+
+  it 'should reject strings without a pair separated by a single letter' do
+    expect(new_nice?('uurcxstgmygtbstg')).to eq false
+  end
+
+  it 'should reject strings without a pair that repeats twice' do
+    expect(new_nice?('ieodomkazucvgmuy')).to eq false
   end
 end
