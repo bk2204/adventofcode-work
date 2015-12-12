8eb53766f41c6ed6ddc79c019310f9bead1a4934
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-12 01:04:57 +0000

    Add part 2 of day 8.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d8_spec.rb
+++ 2015/spec/d8_spec.rb
@@ -2,21 +2,35 @@ require 'rspec'
 load File.join(File.dirname(__FILE__), '..', 'd8')
 
 describe AdventOfCode::D8::Processor do
-  it 'should produce the right values for each wire' do
+  data = [
+    ['""', 2, 0, 6],
+    ['"abc"', 5, 3, 9],
+    ['"aaa\"aaa"', 10, 7, 16],
+    ['"\x27"', 6, 1, 11]
+  ]
+
+  it 'should produce the right values for each string' do
     p = AdventOfCode::D8::Processor.new
-    data = [
-      ['""', 2, 0],
-      ['"abc"', 5, 3],
-      ['"aaa\"aaa"', 10, 7],
-      ['"\x27"', 6, 1]
-    ]
-    data.each do |(s, code, text)|
+    data.each do |(s, code, text, encoded)|
       expect(p.as_code(s)).to eq code
       expect(p.as_text(s)).to eq text
+      expect(p.encode(s).length).to eq encoded
     end
+  end
+
+  it 'should produce the right total difference' do
+    p = AdventOfCode::D8::Processor.new
     count = data.map { |a| a[0] }.reduce(0) do |memo, s|
       memo + p.as_code(s) - p.as_text(s)
     end
     expect(count).to eq 12
+  end
+
+  it 'should produce the right total difference for encoded strings' do
+    p = AdventOfCode::D8::Processor.new
+    count = data.map { |a| a[0] }.reduce(0) do |memo, s|
+      memo + p.encode(s).length - p.as_code(s)
+    end
+    expect(count).to eq 19
   end
 end
