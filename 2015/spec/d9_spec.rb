ac810c76c1ea4757a69cdc51e5f040bf5b7b6b02
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-15 01:43:39 +0000

    Add part 2 of day 9.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d9_spec.rb
+++ 2015/spec/d9_spec.rb
@@ -11,5 +11,7 @@ describe AdventOfCode::D9::GraphTraverser do
     EOM
     text.each_line { |line| p.parse_path(line) }
     expect(p.find_shortest_path).to eq 605
+    expect(p.find_path(:<)).to eq 605
+    expect(p.find_path(:>)).to eq 982
   end
 end
