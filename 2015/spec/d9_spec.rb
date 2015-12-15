1aeeb3707f383ff3d746836ba4d8d2c14f90b571
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-15 01:38:21 +0000

    Add part 1 of day 9.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ 2015/spec/d9_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+load File.join(File.dirname(__FILE__), '..', 'd9')
+
+describe AdventOfCode::D9::GraphTraverser do
+  it 'should produce the right values for a sample path' do
+    p = AdventOfCode::D9::GraphTraverser.new
+    text = <<-EOM.gsub(/^\s+/, '')
+    London to Dublin = 464
+    London to Belfast = 518
+    Dublin to Belfast = 141
+    EOM
+    text.each_line { |line| p.parse_path(line) }
+    expect(p.find_shortest_path).to eq 605
+  end
+end
