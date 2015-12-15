97cb8d6a98e747907a2730a688f40d158860793f
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-15 02:56:11 +0000

    Add part 2 of day 12.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d12_spec.rb
+++ 2015/spec/d12_spec.rb
@@ -7,7 +7,7 @@ describe AdventOfCode::D12::Extractor do
     [
       ['[1,2,3]', 6],
       ['{"a":2,"b":4}', 6],
-      ['[[[3]]', 3],
+      ['[[[3]]]', 3],
       ['{"a":{"b":4},"c":-1}', 3],
       ['{"a":[-1,1]}', 0],
       ['[-1,{"a":1}]', 0],
@@ -15,6 +15,24 @@ describe AdventOfCode::D12::Extractor do
       ['{}', 0]
     ].each do |(s, total)|
       expect(p.sum(s)).to eq total
+    end
+  end
+
+  it 'should compute the correct value for second sum' do
+    p = AdventOfCode::D12::Extractor.new
+    [
+      ['[1,2,3]', 6],
+      ['[1,{"c":"red","b":2},3]', 4],
+      ['{"d":"red","e":[1,2,3,4],"f":5}', 0],
+      ['[1,"red",5]', 6],
+      ['[[[3]]]', 3],
+      ['{"a":{"b":4},"c":-1}', 3],
+      ['{"a":[-1,1]}', 0],
+      ['[-1,{"a":1}]', 0],
+      ['[]', 0],
+      ['{}', 0]
+    ].each do |(s, total)|
+      expect(p.second_sum(s)).to eq total
     end
   end
 end
