b85b29f4a3feb8fbf2ee4f2cfc5893fe9be224f6
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-20 13:03:08 +0000

    Add part 2 of day 18.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d18_spec.rb
+++ 2015/spec/d18_spec.rb
@@ -29,4 +29,42 @@ describe AdventOfCode::D18::LightArray do
     la.next!
     expect(la.count).to eq 4
   end
+
+  it 'should produce the right number of lights in stuck mode' do
+    la = AdventOfCode::D18::LightArray.new(6, true)
+    la.load_state(example)
+    expect(la.count).to eq 17
+  end
+
+  it 'should progress states properly' do
+    la = AdventOfCode::D18::LightArray.new(6, true)
+    la.load_state(example)
+    expect(la.to_s).to eq <<-EOM.gsub(/^\s+/, '')
+    ##.#.#
+    ...##.
+    #....#
+    ..#...
+    #.#..#
+    ####.#
+    EOM
+    expect(la.count).to eq 17
+    la.next!
+    expect(la.to_s).to eq <<-EOM.gsub(/^\s+/, '')
+    #.##.#
+    ####.#
+    ...##.
+    ......
+    #...#.
+    #.####
+    EOM
+    expect(la.count).to eq 18
+    la.next!
+    expect(la.count).to eq 18
+    la.next!
+    expect(la.count).to eq 18
+    la.next!
+    expect(la.count).to eq 14
+    la.next!
+    expect(la.count).to eq 17
+  end
 end
