36447ed35819b77c43c51c1f070c7d2e66395553
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-20 09:47:30 +0000

    Add part 2 of day 15.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d15_spec.rb
+++ 2015/spec/d15_spec.rb
@@ -27,12 +27,22 @@ describe AdventOfCode::D15::Optimizer do
     expect(cookie.space).to eq 56
   end
 
+  it 'should compute calories correctly' do
+    values = {
+      ingred[0] => 40,
+      ingred[1] => 60
+    }
+    cookie = AdventOfCode::D15::Cookie.new(values)
+    expect(cookie.calories).to eq 500
+  end
+
   it 'should compute the correct values for two ingredients' do
-    ingred = [
-      AdventOfCode::D15::Ingredient.new('Butterscotch', -1, -2, 6, 3, 8),
-      AdventOfCode::D15::Ingredient.new('Cinnamon', 2, 3, -2, -1, 3)
-    ]
     opt = AdventOfCode::D15::Optimizer.new(ingred)
     expect(opt.optimize).to eq 62842880
+  end
+
+  it 'should compute the correct values for two ingredients and 500 calories' do
+    opt = AdventOfCode::D15::Optimizer.new(ingred, 500)
+    expect(opt.optimize).to eq 57600000
   end
 end
