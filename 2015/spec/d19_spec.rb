df822fa9efa4e67f0c62170c0dd61dbe6c422b01
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-23 20:15:07 +0000

    Add part 2 of day 19.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d19_spec.rb
+++ 2015/spec/d19_spec.rb
@@ -20,4 +20,17 @@ describe AdventOfCode::D19::Replacer do
     results = rep.combinations('HOHOHO')
     expect(results.length).to eq 7
   end
+
+  it 'should count steps properly' do
+    new_rules = <<-EOM.gsub(/^\s+/, '')
+    e => H
+    e => O
+    H => HO
+    H => OH
+    O => HH
+    EOM
+    rep = AdventOfCode::D19::Replacer.parse(new_rules)
+    expect(rep.derive('HOH')).to eq 3
+    expect(rep.derive('HOHOHO')).to eq 6
+  end
 end
