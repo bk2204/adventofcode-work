5a01d5b4227c54e5a0ab947955a3895a3bc0f1b4
brian m. carlson <sandals@crustytoothpaste.net>
2015-12-24 18:16:59 +0000

    Add part 2 of day 20.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2015/spec/d20_spec.rb
+++ 2015/spec/d20_spec.rb
@@ -3,7 +3,12 @@ load File.join(File.dirname(__FILE__), '..', 'd20')
 
 describe AdventOfCode::D20::PresentDeliverer do
   it 'should produce the proper number of presents' do
-    pd = AdventOfCode::D20::PresentDeliverer.new
+    pd = AdventOfCode::D20::PresentDeliverer.new(10)
     expect(pd.take(9)).to eq [10, 30, 40, 70, 60, 120, 80, 150, 130]
+  end
+
+  it 'should produce the proper number of presents with a limit' do
+    pd = AdventOfCode::D20::PresentDeliverer.new(10, 3)
+    expect(pd.take(8)).to eq [10, 30, 40, 60, 50, 110, 70, 120]
   end
 end
