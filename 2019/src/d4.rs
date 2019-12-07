f98ac0ed63b63231f0ab6ecd8a1219f2a29173e6
brian m. carlson <sandals@crustytoothpaste.net>
2019-12-07 11:50:54 +0000

    Add part 2 of day 4 2019.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- 2019/src/d4.rs
+++ 2019/src/d4.rs
@@ -3,6 +3,7 @@ use adventofcode::d4::Filter;
 use std::io;
 
 fn main() -> io::Result<()> {
-    println!("{}", (240_298..784_956).filter(|&n| Filter::validate(n)).count());
+    println!("{}", (240_298..784_956).filter(|&n| Filter::validate_p1(n)).count());
+    println!("{}", (240_298..784_956).filter(|&n| Filter::validate_p2(n)).count());
     Ok(())
 }
--- 2019/src/lib/d4/mod.rs
+++ 2019/src/lib/d4/mod.rs
@@ -1,7 +1,9 @@
+use std::collections::BTreeMap;
+
 pub struct Filter {}
 
 impl Filter {
-    pub fn validate(n: usize) -> bool {
+    pub fn validate_p1(n: usize) -> bool {
         let s = format!("{}", n);
         // Is this a six-digit number?
         if s.len() != 6 {
@@ -25,6 +27,39 @@ impl Filter {
         }
         true
     }
+
+    pub fn validate_p2(n: usize) -> bool {
+        let s = format!("{}", n);
+        // Is this a six-digit number?
+        if s.len() != 6 {
+            return false;
+        }
+        // Do the digits never decrease?
+        if !s
+            .chars()
+            .fold((' ', true), |(last, acc), c| (c, c >= last && acc))
+            .1
+        {
+            return false;
+        }
+        // Is there at least one consecutive pair of digits that isn't part of a triplet?
+        let is_pair = !s
+            .chars()
+            .fold(BTreeMap::<char, usize>::new(), |mut map, c| {
+                if let Some(v) = map.get_mut(&c) {
+                    *v += 1;
+                } else {
+                    map.insert(c, 1);
+                };
+                map
+            })
+            .iter()
+            .any(|(_, &v)| v == 2);
+        if is_pair {
+            return false;
+        }
+        true
+    }
 }
 
 #[cfg(test)]
@@ -32,10 +67,19 @@ mod tests {
     use super::Filter;
 
     #[test]
-    fn validate() {
-        assert_eq!(Filter::validate(111111), true);
-        assert_eq!(Filter::validate(223450), false);
-        assert_eq!(Filter::validate(123789), false);
-        assert_eq!(Filter::validate(123788), true);
+    fn validate_p1() {
+        assert_eq!(Filter::validate_p1(111111), true);
+        assert_eq!(Filter::validate_p1(223450), false);
+        assert_eq!(Filter::validate_p1(123789), false);
+        assert_eq!(Filter::validate_p1(123788), true);
+    }
+
+    #[test]
+    fn validate_p2() {
+        assert_eq!(Filter::validate_p2(111111), false);
+        assert_eq!(Filter::validate_p2(112233), true);
+        assert_eq!(Filter::validate_p2(123444), false);
+        assert_eq!(Filter::validate_p2(111122), true);
+        assert_eq!(Filter::validate_p2(111223), true);
     }
 }
